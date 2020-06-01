9c78854117688d14b46cc3f1fa7cf9e85c86f1d4
honnage <hon_da-1998@hotmail.com>
2020-06-01 22:55:12 +0700

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                     <ul class="navbar-nav mr-auto">
                         <ul class="navbar-nav mr-auto">
                             {{-- @if(Auth::user()->checkIsStatus()) --}}
-                            @if(Auth::user()->StatusID == 1)
+                            @if(Auth::user()->id == 1)
                                 <li class="nav-item">
                                     <a class="nav-link" href="">{{ ('ตรวจเอกสาร') }}</a>
                                 </li>
